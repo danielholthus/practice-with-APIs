6c35659ec441244d9b7a635e21cb1b36f94aec3f
Daniel Holthus <58455051+danielholthus@users.noreply.github.com>
2022-06-08 18:11:40 -0600

    twilio integration

--- apex-notifications.js
+++ apex-notifications.js
@@ -1,19 +1,20 @@
-// const accountSid = process.env.ACee6c608b571621cd9cea06253738bbb2;
-// const authToken = process.env.66fc6bd3157954cc59595a7b08898eec;
-// const client = require('twilio')(accountSid, authToken);
-
-// client.messages
-//   .create({
-//      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//      from: '+16073005670',
-//      to: '+16207555383'
-//    })
-//   .then(message => console.log(message.sid));
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+const axios = require('axios');
 
 const fetchMapData = () => {
-    fetch('https://api.mozambiquehe.re/maprotation?auth=a15c51c67f777eddc9ec7a4f5d3d1d1e').then(resp => {
-        resp.json().then(data => {
-            console.log(data);
-        });
+    axios.get('https://api.mozambiquehe.re/maprotation?auth=a15c51c67f777eddc9ec7a4f5d3d1d1e').then(resp => {
+        console.log(resp.data);
     });
 };
+
+mapData = fetchMapData().data;
+
+client.messages
+  .create({
+     body: 'This is a test run! OOGABOOGABOO',
+     from: '+16073005670',
+     to: '+16207555383'
+   })
+  .then(message => console.log(message.sid));
