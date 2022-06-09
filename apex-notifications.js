9f3aa7d128e8d3afb15dc26caa531de4384f1390
Daniel Holthus <58455051+danielholthus@users.noreply.github.com>
2022-06-08 18:48:54 -0600

    Update apex-notifications.js

--- apex-notifications.js
+++ apex-notifications.js
@@ -2,19 +2,28 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const axios = require('axios');
+const numbers = ['+16207555383', '+18317470619'];
 
 const fetchMapData = () => {
     axios.get('https://api.mozambiquehe.re/maprotation?auth=a15c51c67f777eddc9ec7a4f5d3d1d1e').then(resp => {
-        console.log(resp.data);
+
+        mapData = resp.data;
+        console.log(mapData);
+
+        // if (mapData.next.map == "World's Edge" && mapData.current.remainingMins <= 30){
+        if (true){
+            for(i in numbers) {
+                client.messages
+                .create({
+                    body: 'Cake time, bucko! World\'s Edge in 30 minutes!',
+                    from: '+16073005670',
+                    to: numbers[i]
+                })
+                .then(message => console.log("Message sent successfully! SID: ", message.sid))
+                .catch(err => console.log(err));
+            }
+        }
     });
 };
 
-mapData = fetchMapData().data;
-
-client.messages
-  .create({
-     body: 'This is a test run! OOGABOOGABOO',
-     from: '+16073005670',
-     to: '+16207555383'
-   })
-  .then(message => console.log(message.sid));
+fetchMapData();
